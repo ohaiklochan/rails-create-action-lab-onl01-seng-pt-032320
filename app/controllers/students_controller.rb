ddec8f53f3b38164c9733a438c71af8bd9ff4dc0
Chloe Steffens <caforonda103@yahoo.com>
2020-08-13 01:04:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
-    
-  end
+    @student = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+  redirect_to post_path(@post)
+end
 
 end
